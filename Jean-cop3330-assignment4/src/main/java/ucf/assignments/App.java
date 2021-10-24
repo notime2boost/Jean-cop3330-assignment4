4d106af276c238015889f04b90a2cf8839697cda
notime2boost <cjean176@gmail.com>
2021-10-24 14:21:48 -0400

    Part1

---
+++ Jean-cop3330-assignment4/src/main/java/ucf/assignments/App.java
@@ -0,0 +1,73 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Christopher Jean
+ */
+
+package ucf.assignments;
+
+class MasterTodoList{
+
+    // List of all current to do list, 100 capacity
+}
+class TodoListInfo {
+
+    // Title , Max Capacity 100 items, due date
+}
+
+class AddTodoList{
+
+    //Makes a new To do list
+}
+
+class RemoveTodoList{
+
+    //Removes a existing To do list
+}
+
+class editTitle{
+
+    //edits title of existing to do list
+}
+
+class addItem{
+
+    //add item name , due date, description
+}
+
+class removeItem{
+
+    //removes item from existing to do list
+}
+
+class editItem{
+
+    // edits item name, or due date, or description
+}
+
+class statusItem{
+
+    // checks status of item if complete or incomplete in a to do list
+}
+
+class saveItems{
+
+    //saves all items in existing to do list
+}
+
+class saveList{
+
+    //saves all list to external location
+}
+
+class loadItems{
+
+    //loads items to a single to do list
+}
+
+class loadList{
+
+    // loads multiple list from external storage
+}
+
+
+
